8dc8d28e96d67ce303aa599405bda7c29ef82dfb
Vanesa Edo <vanesa.en@gmail.com>
2024-02-09 23:14:13 +0100

    1 to 12 exercises completed, not tested, just written.

---
+++ index.js
@@ -0,0 +1,42 @@
+//VARIABLES
+
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = true;
+const PI = 3.14;
+const TAU = PI*2;
+
+//BOOLEANOS
+
+let booleanoAnd = (booleano1 == booleano2);
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 && !=booleano2);
+
+let incrementarDesp = 2;
+resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2;
+resultadoAntes = ++2;
+
+//BUCLES
+let contarHasta10_2 = 0;
+for (let i = 0; i <= 10; i++){
+    contarHasta10_2 === 10;
+}
+
+let postI = 0;
+let postJ = 0;
+for (let i; i <=10; i++){
+    postI = postI + postJ;
+}
+
+let sumarPares = 0;
+for (let i = 0; i < 10; i++){
+    if (i%2 == 0){
+        sumarPares = sumarPares + i;
+    }
+}
+
+//ITERACIONES: european_castle
+
+//VARIABLES
+
